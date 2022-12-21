d66f53e93b92ea2ce57a43e3b24286d39c8f1c75
henry-dura <henryikemefuna@gmail.com>
2022-12-21 10:59:35 +0100

    fix Eslint errors

--- javascript.js
+++ javascript.js
@@ -4,20 +4,18 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const navBar = document.querySelector('.nav');
 const closeIcon = document.querySelector('.close');
 
-
-function displayMenu(){
-    navBar.classList.add('non-display');
-    mobileMenu.classList.remove('non-display');
-    navList.classList.remove('desktop');
-    mobileMenu.appendChild(navList);  
+function displayMenu() {
+  navBar.classList.add('non-display');
+  mobileMenu.classList.remove('non-display');
+  navList.classList.remove('desktop');
+  mobileMenu.appendChild(navList);
 }
 
-function close(){
-    navBar.classList.remove('non-display');
-    mobileMenu.classList.add('non-display');
+function close() {
+  navBar.classList.remove('non-display');
+  mobileMenu.classList.add('non-display');
 }
-
 
 closeIcon.addEventListener('click', close);
-navList.addEventListener('click',close);
+navList.addEventListener('click', close);
 hamburgerIcon.addEventListener('click', displayMenu);
